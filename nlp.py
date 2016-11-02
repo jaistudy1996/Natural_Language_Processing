cf56b797fb19da1a46fed6fa1c6a44810d95f6d2
mhilpl <mhilp001@plattsburgh.edu>
2016-11-02 09:08:15 -0400

    Add code to open test.csv and print out the comments

--- nlp.py
+++ nlp.py
@@ -1,11 +1,15 @@
 # Authors: Jayant Arora, Marie Hilpl
 # This file will have most of the nltk code
 
-import nltk
+import nltk, csv
 
 
 def main():
 	print("NLTK imported successfully.")
+	with open('test.csv', newline='') as csvfile:
+	test = csv.reader(csvfile)
+	for row in test:
+		print(row[2])
 
 
 main()	
