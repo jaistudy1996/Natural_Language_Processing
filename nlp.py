5ec7a3925ad11a29472f071f9401ebd40ac88763
mhilpl <mhilp001@plattsburgh.edu>
2016-11-04 20:43:19 -0400

    tokenize each comment and pos tag each word

--- nlp.py
+++ nlp.py
@@ -2,15 +2,18 @@
 # This file will have most of the nltk code
 
 import nltk, csv
-
+from nltk.tokenize import word_tokenize
 
 def main():
 	print("NLTK imported successfully.")
 	with open('test.csv', newline='') as csvfile:
 	test = csv.reader(csvfile)
 	for row in test:
-		print(row[2])
-
+		#print(row[2])
+		#Tokenize each comment
+		tokens = nltk.word_tokenize(row[2])
+		#Assign POS tag to each word in the comment
+		pos = nltk.pos_tag(tokens)
 
 main()	
 
