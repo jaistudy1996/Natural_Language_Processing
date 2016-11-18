172e9fb161e36dc0fd4a587c02334bceb15e8539
Jayant Arora <jaistudy1996@gmail.com>
2016-11-18 11:18:56 -0500

    Fix IO on closed file bug

--- nlp.py
+++ nlp.py
@@ -7,14 +7,12 @@ from nltk.tokenize import word_tokenize
 def main():
 	print("NLTK imported successfully.")
 	with open('test.csv', newline='') as csvfile:
-	test = csv.reader(csvfile)
-	for row in test:
-		#print(row[2])
-		#Tokenize each comment
-		tokens = nltk.word_tokenize(row[2])
-		#Assign POS tag to each word in the comment
-		pos = nltk.pos_tag(tokens)
+		test = csv.reader(csvfile)
+		for row in test:
+			#print(row[2])
+			#Tokenize each comment
+			tokens = nltk.word_tokenize(row[2])
+			#Assign POS tag to each word in the comment
+			pos = nltk.pos_tag(tokens)
 
 main()	
-
-	
