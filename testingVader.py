8d5fb89b9507ccbaae6413b8098f1cf92d276d6b
mhilpl <mhilp001@plattsburgh.edu>
2016-11-18 09:47:29 -0500

    correctly initalize sentiment analyzer

--- testingVader.py
+++ testingVader.py
@@ -1,18 +1,20 @@
 import nltk, csv
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as sia
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
 def main():
     #Open file
     with open('train.csv', newline='') as csvfile:
         test = csv.reader(csvfile)
+        sid = SIA()
         for row in test:
             comment = row[2]
             print(row[2])
-            ss = sia.polarity_scores(comment)
+            ss = sid.polarity_scores(comment)
             for k in sorted(ss):
                 print('{0}: {1}, '.format(k, ss[k]), end='')
-            print()
+            print("\n")
             
 
 main()
+
